c5132232f916c261ef6c9be80b43a2733fa541a9
infoskill-wd <107102966+infoskill-wd@users.noreply.github.com>
2022-06-08 10:29:00 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const anchors = document.querySelectorAll("a[href*='#']");
+
+for (let anchor of anchors) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const blockId = anchor.getAttribute("href");
+    document.querySelector("" + blockId).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
